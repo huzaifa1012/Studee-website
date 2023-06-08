773b93e9b4d33ac7a26b834005d1bbc419f38f1a
huzaifa1012 <www.pakteach@gmail.com>
2023-06-08 17:58:07 +0500

    example home buttons space added

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -4,16 +4,23 @@ const Home = () => {
   return (
     <>
       <h1>Welcome To Studee.com</h1>
-      <div style={{display:'flex' ,justifyContent:'space-around',borderBottom:'2px solid black',padding:10}}>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
-      <Link to="/contact">
-        <button>Contact</button>
-      </Link>
-      <Link to="/about">
-        <button>About us</button>
-      </Link>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          borderBottom: "2px solid black",
+          padding: 10,
+        }}
+      >
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+        <Link to="/contact">
+          <button>Contact</button>
+        </Link>
+        <Link to="/about">
+          <button>About us</button>
+        </Link>
       </div>
     </>
   );
