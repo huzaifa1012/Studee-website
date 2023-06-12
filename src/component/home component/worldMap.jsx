57d226149376dfc312e200f75675cc57a8ead2e5
huzaifa1012 <www.pakteach@gmail.com>
2023-06-12 11:54:01 +0500

    brand logo sectionn added at the bottom of world map

--- src/component/home component/worldMap.jsx
+++ src/component/home component/worldMap.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./world_map.css"
 function worldMap() {
     return (
+        <>
         <div className='world_map_wrap'>
             <div className='world_map_head'>
                 <h1>We match students with <br />
@@ -12,6 +13,23 @@ function worldMap() {
                 <img src='https://images.studee.com/images/content/content__home--map.png?w=1184&auto=format&q=30&fit=crop' />
             </div>
         </div>
+        <div className="world_map_brands_wrap">
+        <div className="world_map_brands">
+            <span className='world_map_brands_image_row2' >
+            <img className='world_map_brand_image' src="https://images.studee.com/images/university/university__concordia-university-of-edmonton--logo.svg" alt="" />
+            <img className='world_map_brand_image' src="https://images.studee.com/images/university/university__avans-university-of-applied-sciences--logo.svg" alt="" />
+            <img className='world_map_brand_image' src="https://images.studee.com/images/university/university__durham-college--logo.svg" alt="" />
+            <img className='world_map_brand_image' src="https://images.studee.com/images/university/university__greenville-university--logo.png?ixlib=js-2.3.2&auto=format&w=170&h=170&crop=fit&q=45" alt="" />
+            </span>
+            <span className='world_map_brands_image_row1' >
+            <img className='world_map_brand_image' src="https://images.studee.com/images/university/university__global-humanistic-university--logo.png?ixlib=js-2.3.2&auto=format&w=170&h=170&crop=fit&q=45" alt="" />
+            <img className='world_map_brand_image' src="https://images.studee.com/images/university/university__central-michigan-university--logo.svg" alt="" />
+            <img className='world_map_brand_image' src="https://images.studee.com/images/university/university__central-queensland-university--logo.svg" alt="" />
+            </span>
+
+        </div>
+        </div>
+        </>
 
     )
 }
