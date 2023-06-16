778e8d4e9073d028ceb28faf7c9cf5374616706c
huzaifa1012 <www.pakteach@gmail.com>
2023-06-16 15:57:32 +0500

    Uni find and Apply card done with responsive

---
+++ src/component/Selected University/Uni_FindAndApplyCard.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Uni_FindAndApplyCard.css";
+const Uni_FindAndApplyCard = () => {
+  return (
+    <>
+      <div className="Uni_FindAndApplyCard_wrap">
+        <div className="Uni_FindAndApplyCard_main">
+          <div className="Uni_FindAndApplyCard_top">
+            
+          </div>
+          <div className="Uni_FindAndApplyCard_bottom">
+            <div className="UniFindAndApply_Main_wrap">
+            <h1>
+              Find & apply to programs at Alliant International University
+            </h1>
+            <p className="ltc">
+              Find programs you love that match your needs & aspirations, where
+              you stand a high chance of acceptance.
+            </p>
+
+            <div className="UniFindAndApply_user-input-section">
+              <div className="theUniFindAndApply_label">
+                <p className="mtc UniFindAndApply_labelP">Subject</p>
+                <input
+                  type="text"
+                  name=""
+                  id="subject"
+                  placeholder="Enter a subject"
+                  className="UniFindAndApply_inpbox-style"
+                />
+              </div>
+              <div className="UniFindAndApplyBtnCover">
+                <button name="" id="" className="UniFindAndApply_btn">
+                  Find Your Perfect Program{" "}
+                  {/* <BiRightArrowAlt className="UniFindAndApplyBtnIcon" size={25} /> */}
+                </button>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Uni_FindAndApplyCard;
--- src/screens/selected university.jsx
+++ src/screens/selected university.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import SelectedUniHero from "../component/Selected University/selecteduniHero.jsx";
 import Whystudee from "../component/home component/whystudee.jsx" 
 import Uni_details_And_Logo from "../component/Selected University/uni_detailsAndLogo.jsx"
+import Uni_FindAndApplyCard from "../component/Selected University/Uni_FindAndApplyCard.jsx"
 const SelectedUniversity = () => {
   return (
     <>
       <SelectedUniHero/>
       <Whystudee/>
       <Uni_details_And_Logo/>
+      <Uni_FindAndApplyCard/>
     </>
   );
 };
