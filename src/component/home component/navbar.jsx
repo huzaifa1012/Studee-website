e6ec38bfb33ddf169519407e2c8ae4d56129e178
huzaifa1012 <www.pakteach@gmail.com>
2023-09-06 16:15:38 +0500

    icon removed from header programs

--- src/component/home component/navbar.jsx
+++ src/component/home component/navbar.jsx
@@ -241,26 +241,7 @@ export function Navbar({ profileData }) {
                         >
                           Programs
                         </NavLink>
-                        <ChevronDownIcon
-                          className={classNames(
-                            open ? "rotate-180" : "",
-                            "h-5 w-5 flex-none"
-                          )}
-                          aria-hidden="true"
-                        />
                       </Disclosure.Button>
-                      <Disclosure.Panel className="mt-2 space-y-2">
-                        {[...products, ...callsToAction].map((item) => (
-                          <Disclosure.Button
-                            key={item.name}
-                            as="a"
-                            href={item.href}
-                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                          >
-                            {item.name}
-                          </Disclosure.Button>
-                        ))}
-                      </Disclosure.Panel>
                     </>
                   )}
                 </Disclosure>
